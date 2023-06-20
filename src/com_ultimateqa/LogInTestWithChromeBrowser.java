7596d171847958c9c4c55e317da9ddf03a0b4bd7
indiamogambo3 <indiamogambo3@gmail.com>
2023-06-20 16:55:17 +0100

    Homework complete

---
+++ src/com_ultimateqa/LogInTestWithChromeBrowser.java
@@ -0,0 +1,67 @@
+package com_ultimateqa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+/**
+ * Project-5 - Project Name: com-ultimateqa
+ * BaseUrl = https://courses.ultimateqa.com/users/sign_in
+ * 1. Setup chrome browser
+ * 2. Open URL
+ * 3. Print the title of the page
+ * 4. Print the current url
+ * 5. Print the page source
+ * 6. Enter the email to email field
+ * 7. Enter the password to password field
+ * 8. Close the browser
+ */
+
+public class LogInTestWithChromeBrowser {
+
+    public static void main(String[] args) {
+
+        //Base URL
+        String baseUrl = "https://courses.ultimateqa.com/users/sign_in";
+
+        //Set up Chrome browser
+        WebDriver driver = new ChromeDriver();
+
+        //Open URL in browser
+        driver.get(baseUrl);
+
+        //Maximise browser
+        driver.manage().window().maximize();
+        //Set Implicit wait to driver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //Get page title
+        String pageTitle = driver.getTitle();
+        System.out.println("Title of page is: " + pageTitle);
+
+        //Get current URL
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("Current URL is: " + currentUrl);
+
+        //Get page source
+        String pageSource = driver.getPageSource();
+        System.out.println("Page source is: " + pageSource);
+
+        //Get email element
+        WebElement email = driver.findElement(By.id("user[email]"));
+        //Type email in email field
+        email.sendKeys("mukz123@gmail.com");
+
+        //Get password element
+        WebElement password = driver.findElement(By.id("user[password]"));
+        //Type password in password field
+        password.sendKeys("Mogambo123");
+
+        //close driver
+        driver.close();
+    }
+
+}
